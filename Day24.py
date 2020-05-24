d7b2b80827906b2bcb53d09b179c6d02c50372b3
Mishra-raja <rajamishra0702@gmail.com>
2020-05-24 16:36:52 +0530

    Construct Binary Search Tree from Preorder Traversal

---
+++ Day24.py
@@ -0,0 +1,12 @@
+class Solution:
+    def bstFromPreorder(self, preorder: List[int]) -> TreeNode:
+        if not preorder: return None
+        root = TreeNode(preorder[0])
+        i = 1
+        while i < len(preorder):
+            if preorder[i] < preorder[0]: i += 1
+            else: break
+        root.left = self.bstFromPreorder(preorder[1:i])
+        root.right = self.bstFromPreorder(preorder[i:])
+        return root
+        
